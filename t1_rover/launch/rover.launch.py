2ec88582d9bb64059496e340293b74aee0b57c95
Brandon Skinner <97356797+I3randon22@users.noreply.github.com>
2024-04-14 14:51:39 +0100

    Made urdf seperate package use, Created imu filter
    Removed unwanted files

--- t1_rover/launch/rover.launch.py
+++ t1_rover/launch/rover.launch.py
@@ -9,44 +9,29 @@ import xacro
 
 def generate_launch_description():
     ld = LaunchDescription()
-
     pkg_name = 't1_rover'
 
     # Declare the argument for use_sim_time
     use_sim_time_arg = DeclareLaunchArgument(
-        'use_sim_time', default_value='true', description='Whether to run in simulation mode'
+        'use_sim_time', default_value='true', description='Whether to run in simulation mode or not'
     )
 
-    # Use xacro to process the files for SIM and not SIM
-    xacro_file_sim = os.path.join(get_package_share_directory(pkg_name), 'urdf/leo_sim.urdf.xacro')
+    # Use xacro to process the files for simulation
+    xacro_file_sim = os.path.join(get_package_share_directory('leo_description'), 'urdf/leo_sim.urdf.xacro')
     robot_description_raw_sim = xacro.process_file(xacro_file_sim).toxml()
 
-    xacro_file = os.path.join(get_package_share_directory(pkg_name), 'urdf/lidar.xacro')
-    robot_description_raw = xacro.process_file(xacro_file).toxml()
-
-    ''' Pass through the package path to directory
-    pkg_share_directory = get_package_share_directory(pkg_name)
-
-    # Set an environment variable to use in the URDF/Xacro file
-    set_pkg_dir_env = SetEnvironmentVariable(
-        'PKG_DIR', pkg_share_directory
-    )
-
-    urdf_file_path = os.path.join(pkg_share_directory, 'urdf', 'robot.urdf.xacro')
-
-    robot_description = Command(['xacro ', urdf_file_path, ' pkg_dir:=', pkg_share_directory])
-    '''
 
     # Include the t1_sim launch file
     sim_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('t1_sim'), 'launch/sim_bringup.launch.py')])
     )
 
+    # Launch lidar package
     rplidar_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('rplidar_ros'), 'launch/rplidar_a2m12_launch.py')])
     )
 
-    # Launch robot state publisher node with simulation
+    # Launch robot state publisher node with simulation (URDF file)
     robot_state_publisher_sim = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
@@ -55,16 +40,7 @@ def generate_launch_description():
         parameters=[{"use_sim_time": True, "robot_description": robot_description_raw_sim}],
     )
 
-    # Launch robot state publisher node without simulation
-    robot_state_publisher = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        name="robot_state_publisher",
-        output="both",
-        parameters=[{"use_sim_time": False, "robot_description": robot_description_raw}],
-    )
-
-    # Rviz node
+    # Launch rviz2 with the nav2 configuration
     node_rviz = Node(
         package='rviz2',
         executable='rviz2',
@@ -72,6 +48,18 @@ def generate_launch_description():
         arguments=['-d', os.path.join(get_package_share_directory(pkg_name), 'rviz', 'nav2.rviz')],
     )
 
+    # Filter imu data
+    imu_filter_node = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='imu_filter_node',
+        output='screen',
+        parameters=[
+            os.path.join(get_package_share_directory(pkg_name), 'config/imu_filter_params.yaml')
+        ],
+    )
+
+    # Filter odometry data, uses imu data
     robot_localization_node = Node(
         package='robot_localization',
         executable='ekf_node',
@@ -83,31 +71,17 @@ def generate_launch_description():
         ],
     )
 
-    # RPLIDAR node definition corrected (removed trailing comma)
-    node_rplidar = Node(
-        package='rplidar_ros',
-        executable='rplidar_composition',
-        name='rplidar_composition',
-        output='screen',
-        parameters=[{
-            'serial_port': '/dev/ttyUSB0',
-            'serial_baudrate': 115200,  # Adjust baudrate as needed
-            'frame_id': 'laser',
-            'inverted': False,
-            'angle_compensate': True,
-        }]
-    )
-
     # Dynamically add actions based on use_sim_time
     def conditionally_launch_nodes(context):
         if context.launch_configurations['use_sim_time'] == 'true':
             return [sim_launch, robot_state_publisher_sim]
         else:
-            return [robot_state_publisher, rplidar_launch]
+            return [rplidar_launch]
 
     # Add actions to LaunchDescription
     ld.add_action(use_sim_time_arg)
     ld.add_action(node_rviz)
+    ld.add_action(imu_filter_node)
     ld.add_action(robot_localization_node)
     ld.add_action(OpaqueFunction(function=conditionally_launch_nodes))
 
